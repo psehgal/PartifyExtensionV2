3d06e39c7a4d92c16f4e61f51bb32845420fb6f0
Parth Sehgal <parth.sehgal@gatech.edu>
2015-04-16 18:45:33 -0400

    removed junk comments and console.logs

--- background.js
+++ background.js
@@ -35,26 +35,19 @@ function(details) {
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     switch(request.type) {
         case "id":
-            console.log("case id");
             var id = request.value;
             spotifyId = id;
             getAccessCode(id, true);
-            //initializeTokens();
-            //logTokens();
-            //updateStatus();
             break;
         case "login":
-            console.log("case login");
             login();
             break;
         case "get-status":
-            console.log("case get-status");
             var res = '<span style="opacity:.5;">connect</span>';
             if (accessCode) {
                 res = accessCode;
             } 
             var onoff = (off == false) ? "Turn Off Partify" : "Turn On Partify";
-            console.log("onoff: " + onoff);
             sendResponse({
                 message: res,
                 onoff: onoff,
@@ -65,28 +58,21 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
                 if (spotifyId)
                     getAccessCode(spotifyId, false);
             }
-            //initializeTokens();
             break;
         case "new-access-code":
-            console.log("case new-access-code");
             var code = request.value;
             removeTab("http://www.partify.club/party/" + accessCode);
             accessCode = code;
             chrome.tabs.create({url:"http://www.partify.club/party/" + accessCode,"active":false});
-            console.log("new access code: " + code);
             break;
         case "toggle":
-            console.log("toggle");
             if (off == false) {
                 off = true;
             } else {
                 off = false;
                 login();
-                //initializeTokens();
                 timeRunningSeconds = 0;
                 errorMessage = "";
-                logTokens();
-                //updateStatus();
                 if (!installed) {
                     installed = true;
                     setTimeout(function() { updateStatus(); }, 1000 * updateStatusInitialDelaySeconds);
@@ -119,7 +105,6 @@ function updateStatus() {
                 if (spotifyId == undefined) {
                     login();
                 }
-                console.log("online");
                 errorMessage = "" ;
                 if (csrf && oauth && accessCode && playlistId) {
                     getStatus();
@@ -130,7 +115,6 @@ function updateStatus() {
                 }
             }
         } else {
-            console.log("offline");
             errorMessage = "internet connection lost"
             off = true;
         }
@@ -154,7 +138,6 @@ function updateTrackId(jsonResponse) {
     if (currentTrack != trackId) {
         refreshed = false;
         currentTrack = trackId;
-        console.log("current track updated: " + currentTrack);
         var playingPosition = jsonResponse["playing_position"];
         var length = jsonResponse["track"]["length"];
         postTrackToPlaylist(playingPosition, length);
@@ -178,31 +161,24 @@ function updateProperty(propertyString, iProperty) {
     if (propertyString == "volume") {
         if (volume != iProperty) {
             volume = iProperty;
-            console.log("volume: " + iProperty);
             changed = true;
         }
     } else if (propertyString == "shuffle") {
         if (shuffle != iProperty) {
             shuffle = iProperty;
-            console.log("shuffle: " + iProperty);
             changed = true;
         }
     } else if (propertyString == "playing") {
         if (playing != iProperty) {
             playing = iProperty;
-            console.log("playing: " + iProperty);
             changed = true;
         }
     } else if (propertyString == "repeat") {
         if (repeat != iProperty) {
             repeat = iProperty;
-            console.log("repeat: " + iProperty);
             changed = true;
         }
     }
-    // if (changed) {
-    //     postProperty(propertyString, iProperty);
-    // }
     return changed;
 }
  
@@ -212,9 +188,8 @@ function postProperty(propertyString, property) {
     var params = propertyString + "=" + property.toString() + "&accessCode=" + accessCode;
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            console.log(xmlhttp.responseText);
         } else {
-            console.log(xmlhttp.responseText);
+            //console.log(xmlhttp.responseText);
         }
     }
     xmlhttp.open("POST", url, true);
@@ -225,12 +200,11 @@ function postProperty(propertyString, property) {
 function postTrackToPlaylist(playingPosition, length) {
     var xmlhttp = new XMLHttpRequest();
     url = "https://partify.herokuapp.com/api/currentSong";
-    // var params = "accessCode=" + accessCode + "&songId=" + '"' + currentTrack + '"';
     var params = "accessCode=" + accessCode + "&songId=" + currentTrack + "&playingPosition=" + playingPosition + "&length=" + length + "&timestamp=" + Date.now() + "&playing=" + playing + "&repeat=" + repeat + "&shuffle=" + shuffle + "&volume=" + volume;
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            console.log(xmlhttp.responseText);
-            console.log("new song posted to partify");
+            //console.log(xmlhttp.responseText);
+            //console.log("new song posted to partify");
         } else if (xmlhttp.readyState == 4 && xmlhttp.status == 404) {
             getAccessCode(spotifyId, false);
         }
@@ -274,7 +248,6 @@ var getAccessCode = function(id, openTabs) {
             var localPlaylistId = jsonResponse["playlistId"];
             playlistId = localPlaylistId;
             accessCode = localAccessCode;
-            console.log("getAccessCode() got accessCode: " + accessCode);
             if (openTabs) {
                 openTabsInitially();
             }
@@ -293,13 +266,9 @@ var refreshPlaylist = function() {
     refreshed = true;
     var starredPlaylistUrl = "spotify:user:" + spotifyId + ":starred";
     var partifyPlaylistUrl = "spotify:user:" + spotifyId + ":playlist:" + playlistId;
-    chrome.tabs.create({"url":starredPlaylistUrl, "active":false}, function(tab){
-        console.log(tab.id);
-    });
+    chrome.tabs.create({"url":starredPlaylistUrl, "active":false}, function(tab){});
     setTimeout(function() {
-        chrome.tabs.create({"url":partifyPlaylistUrl,"active":false}, function(tab){
-            console.log(tab.id);
-        });
+        chrome.tabs.create({"url":partifyPlaylistUrl,"active":false}, function(tab){});
     }, 500);
     setTimeout(function() { removeTab(starredPlaylistUrl) }, 1000);
     setTimeout(function() { removeTab(partifyPlaylistUrl) }, 1000);
@@ -312,48 +281,34 @@ var getStatus = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             var jsonResponse = JSON.parse(xmlhttp.responseText);
             if (jsonResponse["error"]) {
-                console.log(xmlhttp.responseText);
                 initializeTokens();
             } else if (jsonResponse["track"]["track_resource"]["uri"]) {
                 var trackUri = jsonResponse["track"]["track_resource"]["uri"];
-                //var localCurrentTrack = trackUri.replace("spotify:track:", "");
                 var localCurrentTrack = trackUri;
                 var updated = updateTrackId(jsonResponse);
-                //console.log(localCurrentTrack);
                 var properties = ["volume", "playing", "shuffle", "repeat"];
                 var length = jsonResponse["track"]["length"];
                 var position = jsonResponse["playing_position"];
                 var percent = (position / length) * 100;
-                // for (var i = 0; i < properties.length; i++) {
-                //     var propertyString = properties[i];
-                //     var iProperty = jsonResponse[propertyString];
-                //     updateProperty(propertyString, iProperty);
-                // }
                 var skipDetected = false;
                 var changed = updateProperties(properties, jsonResponse);
                 var skipThreshold = (updateIntervalSeconds / length) * 100.00;
                 if (currentPosition) {
                     var delta = Math.abs(percent - currentPosition)
                     if (delta > (skipThreshold * skipThresholdMultiplier)) {
-                        console.log("skip detected!");
                         skipDetected = true;
-                        //postTrackToPlaylist(position, length);
                     }
                 }
                 if (!updated && (changed || skipDetected)) {
-                    //post to /api/currentSong
-                    console.log("change detected");
                     postTrackToPlaylist(position, length);
                 }
                 currentPosition = percent;
                 if (percent >= refreshThreshold && refreshed == false) {
-                    //console.log("about to call refreshPlaylist()");
                     //refreshPlaylist();
                 }
             }
         }
         else if ((xmlhttp.readyState == 4 && xmlhttp.status == 0) || (xmlhttp.readyState == 0 && xmlhttp.status == 0)) {
-            console.log("status update error");
             xmlhttp.abort();
         }
     }
@@ -364,10 +319,8 @@ var getStatus = function() {
 var getOauth = function() {
     var xmlhttp = new XMLHttpRequest();
     url = "http://open.spotify.com/token";
-    console.log(url);
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            console.log(xmlhttp.responseText);
             var jsonResponse = JSON.parse(xmlhttp.responseText);
             oauth = jsonResponse["t"];
         }
@@ -379,10 +332,8 @@ var getOauth = function() {
 var getCsrf = function() {
     var xmlhttp = new XMLHttpRequest();
     url = "https://" + makeid(10) + ".spotilocal.com:" + port.toString() + "/simplecsrf/token.json?service=remote";
-    console.log(url);
     xmlhttp.onreadystatechange = function(){
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            console.log(xmlhttp.responseText);
             var jsonResponse = JSON.parse(xmlhttp.responseText);
             csrf = jsonResponse["token"];
             return;
